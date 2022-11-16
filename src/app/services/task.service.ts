34cb439da4744942a4c3a3d5eb557122c44c3f42
Paul Thomas <paulthomas2107@gmail.com>
2022-11-16 19:59:17 +0000

    Persist update

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -3,6 +3,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs'
 import {Task} from '../Task'
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'content-type' : 'application/json'
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,5 +25,10 @@ export class TaskService {
   deleteTask(task: Task): Observable<Task> {
     const url = `${this.apiURL}/${task.id}`;
     return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task) : Observable<Task> {
+    const url = `${this.apiURL}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions)
   }
 }
